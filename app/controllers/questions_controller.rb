51a73c904173ea6aede13c93e59ff5df4cf5675d
naoyasakurai0705 <naoya.sakurai.0705@gmail.com>
2018-09-04 16:30:25 +0900

    add_view_new_index_question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,19 @@
 class QuestionsController < ApplicationController
+  def index
+   @test = 'hello'
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:title, :body, :genre).merge(user_id: current_user.id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root  'questions#index'                       #ルートパスの指定
+  resources :questions, only: [:index, :new, :create]
 end
