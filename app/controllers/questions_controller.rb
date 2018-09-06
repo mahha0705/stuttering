dbeb0ee6af9bd433610cc97fd92e2daf149a64a3
naoyasakurai0705 <naoya.sakurai.0705@gmail.com>
2018-09-06 12:08:15 +0900

    add_form_question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,11 +9,11 @@ class QuestionsController < ApplicationController
 
   def create
     @question = Question.create(question_params)
+    redirect_to :root
   end
 
   private
     def question_params
       params.require(:question).permit(:title, :body, :genre).merge(user_id: current_user.id)
     end
-
 end
