c674b0888d538c33812f2b400399729d43fad080
Haya4Taka <takahiro@hayashitakahironoMacBook-ea.local>
2018-10-12 13:53:38 +0900

    Set_valitation_for_presence_of_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          # :confirmable
+
+  #Validation
+  # Overwriting Devise method
+  validates_presence_of :name, if: :name_required?
+
   has_many :questions
   has_many :answers
   has_many :comments
@@ -11,5 +16,11 @@ class User < ApplicationRecord
   has_many :questions, through: :question_likes
   has_many :answer_likes
   has_many :answers, through: :answer_likes
+
+private
+
+  def name_required?
+    true
+  end
 
 end
