b11b8e205e189705877fba5760b7de95f9fd05d6
naoyasakurai0705 <naoya.sakurai.0705@gmail.com>
2018-10-16 07:46:12 +0900

    modify_sign_in_view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @questions = Question.where(user_id: @user.id)
+    @my_answer_questions = Question.joins(:answers).where(user_id: @user.id)
   end
   def edit
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,3 @@
 module ApplicationHelper
-  def check_box_judge(key)
-    if params[:seacrh_sort].nil?
-      return false
-    elsif params[:seacrh_sort].include?(key)
-      return true
-    end
-  end
+
 end
