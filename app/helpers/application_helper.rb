e14a2d6484c02e7c32a9d093e871f48ebe9bffe4
naoyasakurai0705 <naoya.sakurai.0705@gmail.com>
2018-10-19 18:04:25 +0900

    add_errormessage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,13 @@
 module ApplicationHelper
 
-  def eroor_messages(column)
+  def error_messages(column)
 
     message_index = resource.errors.details.find_index {|key, value| key == column.to_sym}
     return if message_index.nil?
-
     message = resource.errors.full_messages[message_index]
-
     html = ""
     html += <<-EOF
-    <div class="alert alert-danger alert-dismissible" role="alert">
+    <div class="alert alert-warning alert-dismissible mt-1" role="alert">
       <button type="button" class="close" data-dismiss="alert">
         <span aria-hidden="true">&times;</span>
         <span class="sr-only">close</span>
@@ -17,8 +15,33 @@ module ApplicationHelper
       #{message}
     </div>
     EOF
-
     html.html_safe
   end
+
+  def question_error_messages(question,column)
+    message_index = question.errors.details.find_index {|key, value| key == column.to_sym}
+    return if message_index.nil?
+    message = question.errors.full_messages[message_index]
+    html = ""
+    html += <<-EOF
+    <div class="alert alert-warning alert-dismissible mt-1" role="alert">
+      <button type="button" class="close" data-dismiss="alert">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">close</span>
+      </button>
+      #{message}
+    </div>
+    EOF
+    html.html_safe
+  end
+
+  def form(qt,k,v,genre=[])
+     form = ""
+     form += <<-EOF
+       #{qt.label :tag, v}
+       #{qt.check_box(:tag,{:checked=> genre.include?(k)? true : false}, k ,false)}
+     EOF
+     form.html_safe
+   end
 
 end
