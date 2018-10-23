d435326f1a86c91110c727f8652606a929bb2b77
naoyasakurai0705 <naoya.sakurai.0705@gmail.com>
2018-10-23 23:59:39 +0900

    add_routing_comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   def create
+    @question = Question.find(params[:question_id])
     @comment = Comment.create(comment_params)
     if @comment.save
-      redirect_to question_path(@comment.answer.question_id) , notice: '投稿完了しました'
+      redirect_to question_path(@question) , notice: '投稿完了しました'
     else
-      @question = Question.find(@comment.answer.question_id)
       @answers = @question.answers
       @answer = Answer.new
       @question_comment = QuestionComment.new
@@ -14,6 +14,6 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:answer_id, :body).merge(user_id: current_user.id)
+      params.require(:comment).permit(:body).merge(user_id: current_user.id ,answer_id: params[:answer_id])
     end
 end
