4ce96ef5cb8f6c90ddf3c67dfd166df0fb3b834a
naoyasakurai0705 <naoya.sakurai.0705@gmail.com>
2018-11-02 19:33:27 +0900

    add_func_pass_change

--- test/mailers/previews/devise_mailer_preview.rb
+++ test/mailers/previews/devise_mailer_preview.rb
@@ -3,4 +3,7 @@ class DeviseMailerPreview < ActionMailer::Preview
   def confirmation_instructions
     Devise::Mailer.confirmation_instructions(User.new(id: 20, name: "サクライ", email: "naoya.sakurai.0705@gmail.com"), Devise.friendly_token[0,20])
   end
+  def reset_password_instructions
+    Devise::Mailer.reset_password_instructions(User.new(id: 20, name: "サクライ", email: "naoya.sakurai.0705@gmail.com"), Devise.friendly_token[0,20])
+  end
 end
