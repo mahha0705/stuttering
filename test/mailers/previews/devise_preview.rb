dc20b3f581d9b44f2290c6a0f199f07ae15dfb3e
naoyasakurai0705 <naoya.sakurai.0705@gmail.com>
2018-11-02 14:26:57 +0900

    modify_mailer_view

--- test/mailers/previews/devise_preview.rb
+++
@@ -1,6 +0,0 @@
-class DevisePreview < ActionMailer::Preview
-  def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(User.new(id: 20, name: "サクライ", email: "naoya.sakurai.0705@gmail.com"), Devise.friendly_token[0,20])
-  end
-end
-
