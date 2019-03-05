35b6dfe20c90a84d9a14f4b8cdf6880b33348d52
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-05 20:39:00 +0900

    FactoryBotの記載を省略する

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,7 +26,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "有効なファクトリを持つこと" do
-    expect(FactoryBot.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it "名前と年齢と性別と吃音の有無があれば、ユーザーが有効になる" do
