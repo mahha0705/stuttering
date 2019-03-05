3d1e2f046315c158d5778aac6ca37abaa500c482
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-05 20:17:45 +0900

    modified syntax errors

--- app/models/question.rb
+++ app/models/question.rb
@@ -12,21 +12,20 @@
 #
 
 class Question < ApplicationRecord
-
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :questionTags, dependent: :destroy
   has_many :questionComments, dependent: :destroy
   has_many :questionLikes, dependent: :destroy
-  has_many :users , through: :questionLikes
+  has_many :users, through: :questionLikes
   has_many :notifications, dependent: :destroy
 
   accepts_nested_attributes_for :questionTags
-  validate :require_any_questionTags
-  def require_any_questionTags
+  validate :require_any_question_tags
+  def require_any_question_tags
     errors.add(:base, :no_questionTag) if questionTags.blank?
   end
-  # validates :questionTag, presence: true
+
   paginates_per 10
 
   validates :title, presence: true
@@ -40,14 +39,14 @@ class Question < ApplicationRecord
   scope :improvement, -> { where(id: QuestionTag.select("question_id").where(tag: 4)) }
   scope :hospital, -> { where(id: QuestionTag.select("question_id").where(tag: 5)) }
   scope :other, -> { where(id: QuestionTag.select("question_id").where(tag: 10)) }
-  scope :new_or_old, -> (time){ order(created_at: time) }
+  scope :new_or_old, ->(time) { order(created_at: time) }
   scope :has_answers, -> { where.not(answers_count: 0) }
   scope :no_answers, -> { where(answers_count: 0) }
-  scope :keyword_search, -> (keyword){where('title LIKE(?) OR body LIKE(?)', "%#{keyword}%","%#{keyword}%")}
-  scope :my_questions, -> (user_id){ where(user_id: user_id) }
-  scope :my_answers_questions, -> (user_id){ where(id: Answer.select("question_id").where(user_id: user_id)) }
+  scope :keyword_search, ->(keyword) { where('title LIKE(?) OR body LIKE(?)', "%#{keyword}%", "%#{keyword}%") }
+  scope :my_questions, ->(user_id) { where(user_id: user_id) }
+  scope :my_answers_questions, ->(user_id) { where(id: Answer.select("question_id").where(user_id: user_id)) }
 
-  def self.select_tag_questions(tags,page)
+  def self.select_tag_questions(tags, page)
     question_ids = QuestionTag.select("question_id").where(tag: tags)
     Question.where(id: question_ids).page(page)
   end
