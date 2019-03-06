a76ec6c670416c10c8521840ae3d1909c436ae29
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-06 17:56:02 +0900

    不要なrequireを削除

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe Notification, type: :model do
   specify { is_expected.to belong_to(:user) }
   specify { is_expected.to belong_to(:notified_by).class_name('User') }
