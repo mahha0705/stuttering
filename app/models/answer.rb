a0e19102c4d0c3c173ffb9a41b75e15fe1ca1cdc
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-06 15:40:51 +0900

    不要なリレーションを削除する

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -18,8 +18,6 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
   has_many :comments, dependent: :destroy
-  has_many :answer_likes, dependent: :destroy
-  has_many :users, through: :answer_likes
 
   counter_culture :question, touch: true
 end
