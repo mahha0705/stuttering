9548076b7e61f2a8e2bc1480904f18473f642a16
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-06 17:46:47 +0900

    Modelのリファクタリング

--- app/models/question_tag.rb
+++ app/models/question_tag.rb
@@ -12,7 +12,13 @@
 class QuestionTag < ApplicationRecord
   belongs_to :question
 
-  enum tag: { school: 0, work: 1, job_hunting: 2, relationship: 3,
-improvement: 4, hospital: 5, other: 10}
-
+  enum tag: {
+    school: 0,
+    work: 1,
+    job_hunting: 2,
+    relationship: 3,
+    improvement: 4,
+    hospital: 5,
+    other: 10
+  }
 end
