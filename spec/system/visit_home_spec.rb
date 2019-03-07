461afe0556d74828504de0201280686fade5ad95
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-07 12:45:56 +0900

    不要なコードを削除

--- spec/system/visit_home_spec.rb
+++ spec/system/visit_home_spec.rb
@@ -1,7 +1,6 @@
 RSpec.describe "VisitHomes", type: :system do
   it "'質問してみる'ボタンがある" do
     visit root_path
-    # expect(page).to have_button?('質問してみる')
     expect(page).to have_link '質問してみる', href: new_user_registration_path
   end
 end
