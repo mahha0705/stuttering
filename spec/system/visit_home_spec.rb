bf2c151e4ad75b7a562d2db328fb404a7f257f35
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-07 12:44:41 +0900

    ボタンからリンクの検証に変更

--- spec/system/visit_home_spec.rb
+++ spec/system/visit_home_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe "VisitHomes", type: :system do
   it "'質問してみる'ボタンがある" do
     visit root_path
-    page.has_button?('質問してみる')
+    # expect(page).to have_button?('質問してみる')
+    expect(page).to have_link '質問してみる', href: new_user_registration_path
   end
 end
