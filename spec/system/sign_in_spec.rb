1c7d8e6536e72ba5d54d045b71a19c7886a2e5d6
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-08 10:46:51 +0900

    サインイン　システムテスト

---
+++ spec/system/sign_in_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe "SignIn", type: :system do
+  let(:user) { create(:user) }
+
+  before do
+    token = user.confirmation_token
+    visit user_confirmation_path(confirmation_token: token)
+    click_link 'ログアウト'
+  end
+
+  it "サインインする" do
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_link 'ログイン'
+    expect(page).to have_content('ログインしました。')
+  end
+end
