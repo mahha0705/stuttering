42e1f9fc09c4104801d75eeb5e9ea4e7583e2287
naoyasakurai <naoyasakurai@pememonombp.lan>
2019-03-08 11:00:26 +0900

    サインインシステムテストのエラー修正

--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "SignIn", type: :system do
     click_link 'ログイン'
     fill_in 'メールアドレス', with: user.email
     fill_in 'パスワード', with: user.password
-    click_link 'ログイン'
+    click_button 'ログイン'
     expect(page).to have_content('ログインしました。')
   end
 end
